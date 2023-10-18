82f9753c8dfd5158cc77eae83c7a60b35fa31662
Kuzoovv <nikitakuzminovsch40@gmail.com>
2023-10-18 17:19:02 +0300

    Add If-else - work,but need check regist

--- script.js
+++ script.js
@@ -1,43 +1,23 @@
 
 
 function playRound(playerSelection, computerSelection) {
-    switch(playRound){
-        case 0:
-            (playerSelection === 'rock' && computerSelection === 'paper')
-        return('You loose! Paper beat rock')
-        break;
-        
-        case 1:
-            (playerSelection === 'scissors' && computerSelection === 'paper')
-        return('You win! Scissors beat paper');
-        break;
-
-        case 2:
-            (playerSelection === 'paper' && computerSelection === 'scissors')
-        return('You loose! Scissors beat paper');
-        break;
-
-        case 3:
-            (playerSelection === 'paper' && computerSelection === 'rock')
-        return('You win! Paper beat rock');
-        break;
-
-        case 4:
-            (playerSelection === 'rock' && computerSelection === 'scissors')
-        return('You win! Rock beat scissors');
-        break;
-
-        case 5:
-            (playerSelection === 'scissors' && computerSelection === 'rock')
-        return('You loose! Rock beat scissors');
-        break;
-
-        case 6:
-            (playerSelection ===  computerSelection)
-        return('Draw');
-        break;
+           if (playerSelection === 'rock' && computerSelection === 'paper'){
+            return('You loose! Paper beat rock');
+        }else if  (playerSelection === 'scissors' && computerSelection === 'paper'){
+            return('You win! Scissors beat paper');
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+            return('You loose! Scissors beat paper');
+        } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+            return('You win! Paper beat rock');
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+            return('You win! Rock beat scissors');
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+            return('You loose! Rock beat scissors');
+        } else if (playerSelection ===  computerSelection){
+            return('Draw');
+        }    
     }
-}
+
  
     
 
@@ -45,18 +25,16 @@ function playRound(playerSelection, computerSelection) {
 
 function getplayerChoice() {
     let result = prompt('Your turn, choose an element')
-    let resultLow = result.toLowerCase();
-    return(resultLow);
+    return(result);
 }
 
 
 function getComputerChoice() {
-    let numbArray = [ 'rock', 'paper', 'scissors'];
+    let numbArray = ['rock', 'paper', 'scissors'];
     let randomElement = numbArray[Math.floor(Math.random()* numbArray.length)];
     return(randomElement);
 } 
 
 let playerSelection = getplayerChoice();
-let computerSelection = getComputerChoice('paper');
-
+let computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
