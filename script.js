f28b0c933852a1f091296f47ac2d315cee0e3474
Kuzoovv <nikitakuzminovsch40@gmail.com>
2023-10-18 12:49:32 +0300

    Add functions getPlrPlay and getCompPlay

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const getComputerChoice = () => {
-    let randomNumber = Math.random(Math.random()*3);
-    switch (randomNumber) {
-        case 0:
-            return (rock);
-        break;
 
-        case 1:
-            return (paper);
-        break;
 
-        case 2:
-            return (scissors);
-        break;    
-
-        console.log(getComputerChoice);
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return('You loose! Paper beat rock')
     }
+       
 }
+
+function getplayerChoice() {
+    let result = prompt('Your turn, choose an element')
+    return(result);
+}
+
+
+function getComputerChoice() {
+    let numbArray = [ 'rock', 'paper', 'scissors'];
+    let randomElement = numbArray[Math.floor(Math.random()* numbArray.length)];
+    return(randomElement);
+} 
+
+let playerSelection = getplayerChoice();
+let computerSelection = getComputerChoice('paper');
+console.log(playRound(playerSelection, computerSelection));
