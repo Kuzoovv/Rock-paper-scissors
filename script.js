211a9c041b0f84e78a2b264f8058d479d778645b
Kuzoovv <nikitakuzminovsch40@gmail.com>
2023-10-27 15:08:52 +0300

    Add +1 to scores

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ let computerScore = 0
 
 function playRound(playerSelection, computerSelection) {
            if (playerSelection === 'rock' && computerSelection === 'paper'){
-            return('You loose! Paper beat rock');
+            computerScore=+1
+            return('You loose! Paper beat rock')
         }else if  (playerSelection === 'scissors' && computerSelection === 'paper'){
+            playerScore=+1
             return('You win! Scissors beat paper');
         } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-            return('You loose! Scissors beat paper');
+            computerScore=+1
+            return('You loose! Scissors beat paper')
         } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+            playerScore=+1
             return('You win! Paper beat rock');
         } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+            playerScore=+1
             return('You win! Rock beat scissors');
         } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-            return('You loose! Rock beat scissors');
+            computerScore=+1
+            return('You loose! Rock beat scissors')
         } else if (playerSelection ===  computerSelection){
             return('Draw');
         }    
@@ -25,6 +31,14 @@ function getplayerChoice() {
     let resultLow = result.toLowerCase();
     return(resultLow);
 }
+
+// try to make functions for playerScore and computerScore
+
+// try add +1 point to function play game
+
+// add цикл to playRound 5 times
+
+// check the code from Odin The Project
 
 
 function getComputerChoice() {
